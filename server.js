def66cc607abfd7bfae298fbc986816ee6a96dfb
panitha90 <31048301+panitha90@users.noreply.github.com>
2017-08-17 00:48:05 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,55 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var page1 = {
+  title: 'Page 1 of Anitha Webapp' ,
+  date: 'Aug 16 2017',
+  heading: 'Into to Webapp',
+  content: `    <p>
+                    Examples of browser applications are simple office software (word processors, online spreadsheets, and presentation tools), but can also include more advanced applications such as project management, computer-aided design, video editing and point-of-sale.
+                </p>`
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+     <html>
+            <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <h1>
+                    ${heading}
+                </h1>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>`;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/page1', function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
+    res.send(createTemplate(page1));
 });
 
 app.get('/page2', function(req,res) {
