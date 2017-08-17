dc0d4f96bbc09be02466bd4bee12eba3b4b22f1a
panitha90 <31048301+panitha90@users.noreply.github.com>
2017-08-18 00:04:44 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,8 +18,8 @@ img.onclick = function () {
 //Applying counter value
 var counter = 0;
 var button = document.getElementById('clickme');
-button.onclick = function(){
+button.onclick = function () {
   counter = counter + 1;  
   var span = document.getElementById('counter');
-  span.innerHTML = counter.toString();
+  span.innerHTML = 7//counter.toString();
 };
