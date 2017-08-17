b775e49567b1828646aa4ba4b6e49af32cddda28
panitha90 <31048301+panitha90@users.noreply.github.com>
2017-08-17 15:15:27 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 console.log('Loaded!');
 
 //change the content of html
-var element = document.getElementById("main-text");
+var element = document.getElementById('main-text');
 element.innerHTML= "I have changed the HTML now";
 
 //move an image
 var image = document.getElementById('madi');
 image.onClick = function(){
-  image.style.marginRight = '100px' ;
+  image.style.marginLeft = '100px' ;
 };
