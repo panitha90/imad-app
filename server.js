d068c484b36d84cf94cba357a76108b2c62d721a
panitha90 <31048301+panitha90@users.noreply.github.com>
2017-08-17 23:53:36 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ function createTemplate(data){
 var counter = 0;
 app.get('/counter',function(req, res){
     counter = counter + 1;
-    res.send(counter.toSting());
+    res.send(counter.toString());
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
