9ccdea31cad15ca36662a7fa320ae4edd418d0fe
panitha90 <31048301+panitha90@users.noreply.github.com>
2017-09-18 00:52:03 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -66,6 +66,7 @@ submit.onclick = function(){
     var username = document.getElementById("username");
     var password = document.getElementById("password");
     request.open('POST','http://panitha90.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', application/json)
     request.send(JSON.stringify({username: username, password: password}));
   
 
