54ec953a2d670d732d50459b5c84aaff10376c85
panitha90 <31048301+panitha90@users.noreply.github.com>
2017-09-17 23:37:05 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var cypto = require('crypto');
+var crypto = require('crypto');
 
 var config = {
     user: 'panitha90',
