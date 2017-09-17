af595e6dc8e459bd771b5b39194d98c9677e983e
panitha90 <31048301+panitha90@users.noreply.github.com>
2017-09-17 23:37:45 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.get('/ui/main.js', function (req, res) {
 
 function hash (input, salt) {
     var hash = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hash.toString(hex);
+    return hash.toString('hex');
 }
 
 app.get('/hash/:input',function (req, res) {
